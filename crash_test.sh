2c92416af8ba9863dc05825a06bf66d9ec725ec1
ZyadGhorab <zyad.ghorab97@gmail.com>
2022-09-24 15:55:05 +0200

    adding clean.sh

--- crash_test.sh
+++ crash_test.sh
@@ -1,7 +1,7 @@
 # Crash test
 ./clean.sh
 java -jar Bully.jar 6 5005 Normal 3000 config1.txt &
-java -jar Bully.jar 5 5004 Crash 3000 config1.txt &
+java -jar Bully.jar 5 5004 Crashed 3000 config1.txt &
 java -jar Bully.jar 4 5003 Normal 3000 config1.txt &
 java -jar Bully.jar 3 5002 Normal 3000 config1.txt Initiator &
 java -jar Bully.jar 2 5001 Normal 3000 config1.txt &
